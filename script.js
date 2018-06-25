864ff1f1521873e2d679e06c4d79ca981d96b8f5
axesve <axesve58@gmail.com>
2018-06-25 20:16:14 +0200

    Comments, attempt to fix 1/2, 1/3 and 1/4 bananas.
    Added comments to code, tried fixing 1/2, 1/3 and 1/4 (should work decently)

--- script.js
+++ script.js
@@ -2,11 +2,25 @@
 //Was going to use Jquery for this but realized it would be a very small project and thus Jquery will slow load speeds...
 
 var banana = 15.24;
+
+//Want to help out? Add more landmarks, objects and things with correct cm height and name!
+//No need to sort them by size, just add at the end and it will sort it self.
+
 var items = [
-{name:"Mount Everest",height:"884800"},
-{name:"Eiffel Tower",height:"324000"},
-{name:"Amazon River",height:"699200000"},];
-var bananas = 0;
+{name:"Mount Everest (H)",height:"884800"},
+{name:"Eiffel Tower (H)",height:"324000"},
+{name:"Amazon River (L)",height:"699200000"},
+{name:"Big Ben (H)",height:"9600"},
+{name:"Leaning Tower of Pisa (H)",height:"5800"},
+{name:"Empire State Building (H)",height:"38100"},
+{name:"Golden Gate Bridge (L)",height:"273700"},
+{name:"Colosseum (L)",height:"18900"},
+{name:"Sagrada Família (H)",height:"17200"},
+{name:"St. Basil's Cathedral (H)",height:"6500"},
+{name:"Taj Mahal (H)",height:"7300"},
+{name:"Kilimanjaro (H)",height:"589500"},
+];
+
 
 window.onload = function () {
 items.sort( function ( a, b ) { return b.height - a.height; } );
@@ -15,49 +29,58 @@ items.sort( function ( a, b ) { return b.height - a.height; } );
 	}
 };
 
+//Spawn items in content div
 function spawnItems(i,n,h){
     var e = document.getElementById("c");
 
     var number = spaceFormat((h/15).toFixed(0));
 	e.insertAdjacentHTML('afterbegin','<div class="item" onclick="toggle(this)"><div class="itemName">'+n+' - '+number+' Bananas</div><div class="itemBananas" id='+i+'>');
 	var tBananas = (h/15240);
-	tBananas--;	
 
 	this.drawBanana(i,0,Math.abs(tBananas));
 	
 }
 
+//Format total banana text from etc. 46000 to 46 000
 function spaceFormat(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
 }
+
 
 function drawBanana(i,b,t) {
   setTimeout(function() {
 
     if (b++ < t) {
+
     	if(b > t){
-	   		if(1 - Math.abs(t-b) >= 0.4){
-		    		var e = document.getElementById(i);
-					e.insertAdjacentHTML('beforeend','<img class="banana" src="imgs/banana2.png">');
-				}else if(1 - Math.abs(t-b) <= 0.4){
+
+    		//Bad way, unsure how well it works.
+    		if(Math.abs(t-b)*1000 < 1000 && Math.abs(t-b)*1000 > 600){
 					var e = document.getElementById(i);
 					e.insertAdjacentHTML('beforeend','<img class="banana" src="imgs/banana3.png">');
-				}else if(1 - Math.abs(t-b) >= 0.7){
-					var e = document.getElementById(i);
+    		}else if(Math.abs(t-b)*1000 <= 500){
+    				var e = document.getElementById(i);
+					e.insertAdjacentHTML('beforeend','<img class="banana" src="imgs/banana2.png">');
+    		}else if(Math.abs(t-b)*1000 < 250){
+    				var e = document.getElementById(i);
 					e.insertAdjacentHTML('beforeend','<img class="banana" src="imgs/banana1.png">');
-				}
+    		}
 	    	}
 
-	    if(t > 0){
-    	var e = document.getElementById(i);
-		e.insertAdjacentHTML('afterbegin','<img class="banana" src="imgs/banana.png">');
-	}
+
+	    	//Check if there is enough for 1 banana.
+	    	if(b > 1){
+		    	var e = document.getElementById(i);
+				e.insertAdjacentHTML('afterbegin','<img class="banana" src="imgs/banana.png">');
+			}
+
 		drawBanana(i,b,t);
 	
     }
   }, 20);
 };
 
+//Toggle show
 function toggle(i){
 	if(i.classList.contains("show")){
 		i.classList.remove("show");
